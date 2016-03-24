0e418902482adefd5a60a667eb95dce9398783a7
brusinow <brusinow@me.com>
2016-03-23 20:06:13 -0700

    finished homework

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,77 @@
 var data = require("./products.json")
 
-// Write your solutions below
+// var count = 0;
+// for (var i = 0; i < data['items'].length; i++){
+// 	if (data['items'][i].kind === 'shopping#product')
+// 		count++;
+//   console.log(data.items[i].product.title);
+// }
 
-console.log(data["items"]);
+// console.log(count);
+
+
+
+
+
+// for (var i = 0; i < data['items'].length; i++){
+//   if (data.items[i].product.inventories[0].availability === "backorder") {
+
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+
+
+// for (var i = 0; i < data['items'].length; i++){
+//   if (data.items[i].product.images.length > 1){
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+
+
+
+  // for (var i = 0; i < data['items'].length; i++){
+  //   if (data.items[i].product.brand === "Canon") {
+  //     console.log(data.items[i].product.title);
+  //   }
+  // }
+
+
+// for (var i = 0; i < data['items'].length; i++){
+//   if (data.items[i].product.author.name.includes("eBay") && data.items[i].product.brand === "Canon"){
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+
+for (var i = 0; i < data['items'].length; i++){
+  console.log(data.items[i].product.brand);
+  console.log(data.items[i].product.inventories[0].price);
+  console.log(data.items[i].product.images[0]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
